9dae1a04ed8c3ef36cce089417ca13a1cbeba867
孔令飞 <colin404@foxmail.com>
2024-05-02 20:26:41 +0800

    chore: update release method

--- scripts/make-rules/release.mk
+++ scripts/make-rules/release.mk
@@ -8,13 +8,9 @@
 
 .PHONY: release.run
 release.run: tools.verify.uplift release._check ## Generate and commit CHANGELOG, then tag the git repo.
-	@uplift release --fetch-all --skip-bumps
+	@uplift release --fetch-all
 
 release._check:
-	@if [[ "$(shell git symbolic-ref --short HEAD)" != "master" ]]; then \
-        echo "Branch format is incorrect. Please switch to master branch"; \
-        exit 1; \
-    fi
 	@if ! git diff --quiet; then \
         echo "Git repository is not clean"; \
         exit 1; \
