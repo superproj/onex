57a6520ec69e94d38f9b6e8580cea33bdcda78e8
孔令飞 <colin404@foxmail.com>
2024-06-11 23:29:02 +0800

    refactor: update verify-codegen.sh

--- scripts/verify-codegen.sh
+++ scripts/verify-codegen.sh
@@ -1,41 +1,29 @@
 #!/usr/bin/env bash
 
-# Copyright 2022 Lingfei Kong <colin404@foxmail.com>. All rights reserved.
-# Use of this source code is governed by a MIT style
-# license that can be found in the LICENSE file. The original repo for
-# this file is https://github.com/superproj/onex.
+# Copyright 2014 The Kubernetes Authors.
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# This script verifies whether a code update is needed.
+# Usage: `scripts/verify-codegen.sh <parameters for update-codegen.sh>`.
 
 set -o errexit
 set -o nounset
 set -o pipefail
 
 ONEX_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+source "${ONEX_ROOT}/scripts/lib/verify-generated.sh"
 
-DIFFROOT="${ONEX_ROOT}/pkg"
-TMP_DIFFROOT="${ONEX_ROOT}/_tmp/pkg"
-_tmp="${ONEX_ROOT}/_tmp"
+export UPDATE_API_KNOWN_VIOLATIONS=true
 
-cleanup() {
-  rm -rf "${_tmp}"
-}
-trap "cleanup" EXIT SIGINT
-
-cleanup
-
-mkdir -p "${TMP_DIFFROOT}"
-cp -a "${DIFFROOT}"/* "${TMP_DIFFROOT}"
-
-"${ONEX_ROOT}/scripts/update-codegen.sh"
-echo "diffing ${DIFFROOT} against freshly generated codegen"
-ret=0
-diff -Naupr "${DIFFROOT}" "${TMP_DIFFROOT}" || ret=$?
-cp -a "${TMP_DIFFROOT}"/* "${DIFFROOT}"
-if [[ $ret -eq 0 ]]
-then
-  echo "${DIFFROOT} up to date."
-else
-  echo "${DIFFROOT} is out of date. Please run hack/update-codegen.sh"
-  exit 1
-fi
+onex::verify::generated "Generated files need to be updated" "Please run 'scripts/update-codegen.sh'" scripts/update-codegen.sh "$@"
