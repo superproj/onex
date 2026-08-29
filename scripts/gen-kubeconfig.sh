a057763377a3f48a83517d3b21965a12361ace17
colin404 <colin404@foxmail.com>
2026-08-29 11:50:16 +0800

    commited at 20260829115016

--- scripts/gen-kubeconfig.sh
+++ scripts/gen-kubeconfig.sh
@@ -15,14 +15,14 @@ ONEX_APISERVER_HOST=${ONEX_APISERVER_HOST:-127.0.0.1}
 ONEX_APISERVER_SECURE_PORT=${ONEX_APISERVER_SECURE_PORT:-52443}
 
 function gen_kubeconfig() {
-  cn=`openssl x509 -in $2 -noout -text|awk -F'CN = ' '/Subject.*CN/{print $NF}'`
+  cn=`openssl x509 -in $2 -noout -subject | awk -F'CN=' '{print $NF}'`
 
 	cat << EOF
 apiVersion: v1
 clusters:
 - cluster:
     server: https://${ONEX_APISERVER_HOST}:${ONEX_APISERVER_SECURE_PORT}
-    certificate-authority-data: `base64 $1 |tr -d '\n'`
+    certificate-authority-data: `base64 < $1 |tr -d '\n'`
   name: ${cn}
 contexts:
 - context:
@@ -35,8 +35,8 @@ preferences: {}
 users:
 - name: ${cn}
   user:
-    client-certificate-data: `base64 $2 |tr -d '\n'`
-    client-key-data: `base64 $3 |tr -d '\n'`
+    client-certificate-data: `base64 < $2 |tr -d '\n'`
+    client-key-data: `base64 < $3 |tr -d '\n'`
 EOF
 }
 
